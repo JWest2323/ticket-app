15375e535dce49edbcf0e1c789a05ecf7206ec79
JWest2323 <thejamalwest@gmail.com>
2023-10-24 18:15:42 -0500

    nav styling & routing

--- app/(components)/Nav.jsx
+++ app/(components)/Nav.jsx
@@ -1,10 +1,15 @@
+import { faTicket, faHome } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Link from "next/link";
 
 const Nav = () => {
-  return <nav>
-    <div>
+  return <nav className="flex justify-between bg-nav p-4">
+    <div className="flex items-center space-x-4">
       <Link href="/">
-          <FontAwesomeIcon
+          <FontAwesomeIcon icon={faHome} className="icon" />
+      </Link>
+      <Link href="/TicketPage/new">
+          <FontAwesomeIcon icon={faTicket} className="icon" />
       </Link>
     </div>
     <div>
